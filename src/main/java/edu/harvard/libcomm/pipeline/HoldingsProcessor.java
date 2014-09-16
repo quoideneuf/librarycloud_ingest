cdf06a02d408c9461721cce7f7328c02aac23aab
Michael Vandermillen <michael_vandermillen@harvard.edu>
2014-09-16 16:35:16 -0400

    add holdings processor class

---
+++ src/main/java/edu/harvard/libcomm/pipeline/HoldingsProcessor.java
@@ -0,0 +1,26 @@
+package edu.harvard.libcomm.pipeline;
+
+import java.io.StringReader;
+import java.util.Iterator;
+import java.util.List;
+
+import edu.harvard.libcomm.message.LibCommMessage;
+import gov.loc.mods.v3.ModsCollection;
+import gov.loc.mods.v3.ModsType;
+
+public class HoldingsProcessor implements IProcessor {
+	
+	public void processMessage(LibCommMessage libCommMessage) {	
+		String data = libCommMessage.getPayload().getData();
+		System.out.println(data);
+		StringReader modsReader = new StringReader(data);
+		ModsCollection modsCollection = MessageUtils.unmarshalMods(modsReader);
+		List<ModsType> modsList =  modsCollection.getMods();
+        Iterator<ModsType> modsIterator = modsList.iterator();
+        while (modsIterator.hasNext()) {
+           List<Object> modsGroup = modsIterator.next().getModsGroup();
+           //System.out.println(modsGroup.get(0).toString());
+        }
+		
+	}
+}
