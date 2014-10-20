bc8360f76da257a353031d7dddc324af4573dc48
Jeffrey Licht <jeff@podconsulting.com>
2014-10-20 17:55:30 -0400

    Finish adding S3 commands

--- util/ingest_aleph.sh
+++ util/ingest_aleph.sh
@@ -1,37 +1,38 @@
- #!/bin/bash
+#!/bin/bash
 
 # Script to kick of an Aleph ingest
 # 
-# Usage: 	ingest-aleph.sh [DATA_FILE] [USERNAME@SERVER] [ssh key path]
-# Example: 	ingest-aleph.sh ab.bib.00.20140808.full.mrc librarycloud@ingest01.librarycloud.lib.harvard.edu ~/.ssh/mykey.pem
+# Usage:    ingest-aleph.sh [DATA_FILE] [SQS_ENVIRONMENT]
+# Example:  ingest-aleph.sh ab.bib.00.20140808.full.mrc test
 # 
 # TODO: Make more robust
 
 SOURCE_FILE_PATH=$1
 SOURCE_FILE_NAME=$(basename $SOURCE_FILE_PATH)
-TARGET_SERVER=$2
-TARGET_COMMAND_DIRECTORY=/var/lib/librarycloud/files/ingest-aleph
-TARGET_FILE_DIRECTORY=/var/lib/librarycloud/files/dropbox
-TARGET_BUCKET=s3://harvard.librarycloud.upload.aleph
-COMMAND_BUCKET=s3://harvard.librarycloud.commandqueue
-SSH_KEY_LOCATION=$3
+SQS_ENVIRONMENT=$2
+TARGET_BUCKET=harvard.librarycloud.upload.$SQS_ENVIRONMENT.aleph
+COMMAND_BUCKET=harvard.librarycloud.command.$SQS_ENVIRONMENT.aleph
 
 if [ $# -ne 2 ]; then
-    echo "Usage: ingest-aleph.sh [DATA_FILE] [ENVIRONMENT]"
+    echo "Usage: ingest-aleph.sh [DATA_FILE] [SQS_ENVIRONMENT]"
     exit 1
 fi
 
-# Create bucket (not a problem if bucket already exists)
-aws s3 mb s3://harvard.librarycloud.upload.aleph
+# Create buckets (it's not a problem if the bucket already exists)
+aws s3 mb s3://$COMMAND_BUCKET
+aws s3 mb s3://$TARGET_BUCKET
 
-# Copy file to target
-aws s3 cp $SOURCE_FILE_PATH $TARGET_BUCKET
+# Copy data file to target
+aws s3 cp $SOURCE_FILE_PATH s3://$TARGET_BUCKET
 
 # Create download URL
-SOURCE_FILE_URL=`sign_s3_url.bash --bucket "$TARGET_BUCKET" --file-path "$SOURCE_FILE_NAME"`
+SOURCE_FILE_URL=`sign_s3_url.bash --bucket $TARGET_BUCKET --file-path $SOURCE_FILE_NAME`
+
+# Need to escape ampersands in the replacement string, or sed will do odd stuff
+SED_FILE_URL=`echo $SOURCE_FILE_URL | sed 's|&|\\\&|g'`
 
 # Create ingest command
-sed -e "s|TARGET|$SOURCE_FILE_URL|" > $SOURCE_FILE_NAME.command.xml <<EOF
+sed -e "s|TARGET|$SED_FILE_URL|" > $SOURCE_FILE_NAME.command.xml <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <lib_comm_message>
     <command>INGEST</command>
@@ -48,6 +49,6 @@ if [ ! -f "$SOURCE_FILE_NAME.command.xml" ]; then
   exit 1
 fi
 
-# Copy ingest command to target
-scp -i $SSH_KEY_LOCATION $SOURCE_FILE_NAME.command.xml $TARGET_SERVER:$TARGET_COMMAND_DIRECTORY
-### TODO: Copy the file to a location where it will be uploaded to a queue
+# Copy ingest command to target queue
+aws sqs create-queue --queue-name=$SQS_ENVIRONMENT-aleph-command
+aws sqs send-message --queue=http://sqs.us-east-1.amazonaws.com/$SQS_ENVIRONMENT-aleph-command --message-body="`cat $SOURCE_FILE_NAME.command.xml`"
