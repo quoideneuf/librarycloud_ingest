e0ec0b1aae6c50361bddac6d196658d70ab09523
Jeffrey Licht <jeff@podconsulting.com>
2014-10-20 17:04:48 -0400

    Beginning to add s3 commands

--- util/ingest_aleph.sh
+++ util/ingest_aleph.sh
@@ -12,18 +12,26 @@ SOURCE_FILE_NAME=$(basename $SOURCE_FILE_PATH)
 TARGET_SERVER=$2
 TARGET_COMMAND_DIRECTORY=/var/lib/librarycloud/files/ingest-aleph
 TARGET_FILE_DIRECTORY=/var/lib/librarycloud/files/dropbox
+TARGET_BUCKET=s3://harvard.librarycloud.upload.aleph
+COMMAND_BUCKET=s3://harvard.librarycloud.commandqueue
 SSH_KEY_LOCATION=$3
 
-if [ $# -ne 3 ]; then
-    echo "Usage: ingest-aleph.sh [DATA_FILE] [USERNAME@SERVER] [ssh key path]"
+if [ $# -ne 2 ]; then
+    echo "Usage: ingest-aleph.sh [DATA_FILE] [ENVIRONMENT]"
     exit 1
 fi
 
+# Create bucket (not a problem if bucket already exists)
+aws s3 mb s3://harvard.librarycloud.upload.aleph
+
 # Copy file to target
-scp -i $SSH_KEY_LOCATION $SOURCE_FILE_PATH $TARGET_SERVER:$TARGET_FILE_DIRECTORY
+aws s3 cp $SOURCE_FILE_PATH $TARGET_BUCKET
+
+# Create download URL
+SOURCE_FILE_URL=`sign_s3_url.bash --bucket "$TARGET_BUCKET" --file-path "$SOURCE_FILE_NAME"`
 
 # Create ingest command
-sed -e "s|TARGET|$TARGET_FILE_DIRECTORY/$SOURCE_FILE_NAME|" > $SOURCE_FILE_NAME.command.xml <<EOF
+sed -e "s|TARGET|$SOURCE_FILE_URL|" > $SOURCE_FILE_NAME.command.xml <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <lib_comm_message>
     <command>INGEST</command>
@@ -42,3 +50,4 @@ fi
 
 # Copy ingest command to target
 scp -i $SSH_KEY_LOCATION $SOURCE_FILE_NAME.command.xml $TARGET_SERVER:$TARGET_COMMAND_DIRECTORY
+### TODO: Copy the file to a location where it will be uploaded to a queue
