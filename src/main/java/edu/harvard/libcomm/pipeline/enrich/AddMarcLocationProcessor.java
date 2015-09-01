f05d6aac8ef6eb776d5e524d0a05af836eb74b2d
Jeffrey Licht <jlicht@podconsulting.com>
2015-09-01 15:13:25 -0400

    Add reference to original MARC records to MODS aleph records

---
+++ src/main/java/edu/harvard/libcomm/pipeline/enrich/AddMarcLocationProcessor.java
@@ -0,0 +1,50 @@
+package edu.harvard.libcomm.pipeline.enrich;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.json.XML;
+
+import edu.harvard.libcomm.message.LibCommMessage;
+import edu.harvard.libcomm.message.LibCommMessage.Payload;
+import edu.harvard.libcomm.pipeline.Config;
+import edu.harvard.libcomm.pipeline.IProcessor;
+import edu.harvard.libcomm.pipeline.MessageUtils;
+
+/**
+ * Base class for processors that call an external service that takes a list of 
+ * record IDs, and then uses the results to transform the records using and XSL
+ * stylesheet.
+ */
+public class AddMarcLocationProcessor implements IProcessor {
+	protected Logger log = Logger.getLogger(AddMarcLocationProcessor.class); 	
+	private String marcBaseUrl;
+
+	public void processMessage(LibCommMessage libCommMessage) throws Exception {	
+		String data = null;
+		try {
+			data = MessageUtils.transformPayloadData(libCommMessage,
+				"src/main/resources/addmarcurl.xsl", 
+				"<marcpath>" + marcBaseUrl + "</marcpath>");
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		
+		Payload payload = new Payload();
+		payload.setData(data);
+        libCommMessage.setPayload(payload);
+	}
+
+	public void setMarcBaseUrl(String s) {
+		this.marcBaseUrl = s;
+	}
+
+	public String getMarcBaseUrl() {
+		return this.marcBaseUrl;
+	}
+}
