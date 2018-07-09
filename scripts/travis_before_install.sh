c35325463e6b47ac85917005c72d08e298a4eb87
Brian Hoffman <brianjhoffman@gmail.com>
2018-07-08 22:51:40 -0400

    fix script

--- scripts/travis_before_install.sh
+++ scripts/travis_before_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp ../src/main/resources//META-INF/persistence.xml.example ../src/main/resources/META-INF/persistence.xml
+cp ./src/main/resources/META-INF/persistence.xml.example ./src/main/resources/META-INF/persistence.xml
