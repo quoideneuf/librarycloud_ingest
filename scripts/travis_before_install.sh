7eed9e5a0096c7c0ede7c4e8d3f38c1be50d08d7
Brian Hoffman <brianjhoffman@gmail.com>
2018-07-08 23:04:37 -0400

    fix script

--- scripts/travis_before_install.sh
+++ scripts/travis_before_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cp ./src/main/resources/META-INF/persistence.xml.example ./src/main/resources/META-INF/persistence.xml
+cp ./src/main/resources/librarycloud.env.properties.example ./src/main/resources/librarycloud.env.properties
